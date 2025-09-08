f82313ea9462e32958457f497acc5b63c3630a5f
youco . <ouichouaniabdlhakim@gmail.com>
2025-09-08 18:49:44 +0100

    start working on the second challenge

---
+++ algo/2_tri_selection.c
@@ -0,0 +1,29 @@
+// Selection Sort is a sorting method where you repeatedly:
+
+// 1. Select the smallest (or largest) element from the unsorted part of the array.
+// 2. Swap it with the first element of the unsorted part.
+// 3. Move the boundary of the sorted part one step forward.
+// 4. Repeat until the whole array is sorted.
+
+#include <stdio.h> 
+
+int main(){
+
+
+    int array [] = {1 , 0 , 52 , 33 , 3 , 22 , 2 } ;
+    int boundary = sizeof(array)/4;
+
+    
+    for(int i = 0 ; i < boundary ; i++){
+        
+
+    }
+}
+
+    for(int i = 0; i < sizeof(array)/4 ; i++){
+        printf("%d " , array[i]);
+    }
+
+
+    return 0 ;
+}
